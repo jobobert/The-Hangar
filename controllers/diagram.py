010c137785c7a9e5fde8cb287700b5ba7dc5a412
Nathan Evans <caelus.app@gmail.com>
2025-08-21 21:06:53 -0500

    Added a basic graph editor

--- controllers/diagram.py
+++ controllers/diagram.py
@@ -1,9 +1,23 @@
 def rendermodeldiagram():
     model = db.model(request.args(0)) 
 
-    return dict(dot=model.diagram)
+    return dict(dot=model.diagram, model_id=model.id)
 
 def rendermodelexport():
     model = db.model(request.args(0)) 
 
     return dict(dot=model.diagram)
+
+def editmodeldiagram():
+    model = db.model(request.args(0)) 
+
+    details_form = SQLFORM(db.model, model.id, fields=[
+                           'diagram'], showid=False, formstyle='divs')
+
+    if details_form.process().accepted:
+        session.flash = "Model Updated"
+        redirect(URL('model', 'index', args=details_form.vars.id, extension="html"))
+    elif details_form.errors:
+        response.flash = "Error Adding New Model"
+
+    return dict(dot=model.diagram, model_name=model.name, form=details_form)
